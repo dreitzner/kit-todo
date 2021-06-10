804882029a31646d5016212ce3fbec1de2a16d91
Antonia Langer <a.langer@emakina.at>
2021-06-10 16:51:30 +0200

    Added signUp page and supabase

---
+++ src/routes/sign_in.svelte
@@ -0,0 +1,35 @@
+<script>
+    
+    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzA1OTI4NSwiZXhwIjoxOTM4NjM1Mjg1fQ.9OiUIjKysUqWz_Y2IToCtMz6Wim2PdM1kq0HalmGsec'
+    const SUPABASE_URL = "https://zyujhjqnioinakawkpfu.supabase.co"
+
+    import { Auth, Typography, Button , Input} from '@supabase/ui'
+    import { createClient } from '@supabase/supabase-js'
+
+    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+    let password = '';
+    let email = '';
+    
+    
+    
+    
+    async function signUp(){
+        const { user, error } = await supabase.auth.signUp({
+            email: email,
+            password: password
+        })
+        console.log(user,error)
+        password = '';
+        email = '';
+    }
+
+</script>
+
+
+<input bind:value={email} placeholder="Gebe hier deine Email ein">
+<input bind:value={password} placeholder="Gehe hier dein Passwort ein">
+
+<button on:click={signUp}>Signup</button>
+
+
