9d89026ada6e19e4947b1fb671ea80581ec4664c
Antonia Langer <a.langer@emakina.at>
2021-06-15 17:23:02 +0200

    Authentication: Inserted SignIn and SignUp -> errors while signing

--- src/routes/sign_in.svelte
+++ src/routes/sign_in.svelte
@@ -3,33 +3,42 @@
     const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzA1OTI4NSwiZXhwIjoxOTM4NjM1Mjg1fQ.9OiUIjKysUqWz_Y2IToCtMz6Wim2PdM1kq0HalmGsec'
     const SUPABASE_URL = "https://zyujhjqnioinakawkpfu.supabase.co"
 
-    import { Auth, Typography, Button , Input} from '@supabase/ui'
     import { createClient } from '@supabase/supabase-js'
 
     const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-    let password = '';
     let email = '';
+    let password = '';
     
-    
-    
-    
-    async function signUp(){
-        const { user, error } = await supabase.auth.signUp({
+    async function signIn(){
+        const { user, session, error } = await supabase.auth.signIn({
             email: email,
-            password: password
-        })
-        console.log(user,error)
-        password = '';
-        email = '';
+            password: password,
+        }) 
     }
-
 </script>
 
 
-<input bind:value={email} placeholder="Gebe hier deine Email ein">
-<input bind:value={password} placeholder="Gehe hier dein Passwort ein">
-
-<button on:click={signUp}>Signup</button>
+<div class="container login-container">
+    <div class="row">
+        <div class="col-md-6 login-form">
+            <h3>Login</h3>
+            <form>
+                <div class="form-group">
+                    <input bind:value={email} type="text" class="form-control" placeholder="Gebe hier deine Email ein">
+                </div>
+                <div class="form-group">
+                    <input bind:value={password} type="text" class="form-control" placeholder="Gebe hier dein Passwort ein">
+                </div>
+                <div class="form-group">
+                    <button on:click={signIn}>Einloggen</button>
+                </div>
+                <div class="form-group">
+                    <a href="/sign_up" class="ForgetPwd">Sie sind noch nicht registriert?</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
 
---
+++ src/routes/sign_up.svelte
@@ -0,0 +1,63 @@
+<script>
+    
+    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzA1OTI4NSwiZXhwIjoxOTM4NjM1Mjg1fQ.9OiUIjKysUqWz_Y2IToCtMz6Wim2PdM1kq0HalmGsec'
+    const SUPABASE_URL = "https://zyujhjqnioinakawkpfu.supabase.co"
+
+    import { createClient } from '@supabase/supabase-js'
+
+    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+    let password = '';
+    let email = '';
+    let firstName = '';
+    let lastName = '';
+    
+    async function signUp(){
+        const { user, error } = await supabase.auth.signUp({
+            email: email,
+            password: password
+        })
+        console.log(user,error)
+        resetParameters()
+    }
+
+    /*async function saveData(){
+        const { data, error } = await supabase
+        .from('users')
+        .insert([
+        { firstName: firstName, lastName: lastName }
+  ])
+    }*/
+
+    async function resetParameters(){
+        password = '';
+        email = '';
+        firstName = '';
+        lastName = '';
+    }
+</script>
+
+<div class="container login-container">
+    <div class="row">
+        <div class="col-md-6 login-form">
+            <h3>Registrieren</h3>
+            <form>
+                <div class="form-group">
+                    <input bind:value={firstName} type="text" class="form-control" placeholder="Vorname">
+                </div>
+                <div class="form-group">
+                    <input bind:value={lastName} type="text" class="form-control" placeholder="Nachname">
+                </div>
+                <div class="form-group">
+                    <input bind:value={email} type="text" class="form-control" placeholder="Gebe hier deine Email ein">
+                </div>
+                <div class="form-group">
+                    <input bind:value={password} type="text" class="form-control" placeholder="Gebe hier dein Passwort ein">
+                </div>
+                <div class="form-group">
+                    <button on:click={signUp}>Registrieren</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
